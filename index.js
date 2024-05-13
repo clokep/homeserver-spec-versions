407e5642d9600943690d640c798be3cee30e3e58
Patrick Cloke <clokep@patrick.cloke.us>
2024-05-13 11:50:47 -0400

    Make the post release option a checkmark.

--- index.js
+++ index.js
@@ -73,45 +73,40 @@ function resetZoom(chartId) {
 }
 
 function build() {
-    // Generate the versions of each chart for each homeserver -- one including
-    // all data, and one including data for only after the homeserver's initial
-    // release.
-    for (const post_rel of [false, true]) {
-        // Bar chart of the number of days between a spec release and the homeserver
-        // supporting it.
-        const barContext = document.getElementById("days-to-support" + (post_rel ? "-post-rel" : ""));
-        new Chart(barContext, {
-            type: "bar",
-            data: null,
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: "Days to support spec version" + (post_rel ? " (post-release)" : ""),
-                    },
-                    zoom: zoomOptions,
-                }
+    // Bar chart of the number of days between a spec release and the homeserver
+    // supporting it.
+    const barContext = document.getElementById("days-to-support");
+    new Chart(barContext, {
+        type: "bar",
+        data: null,
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Days to support spec version",
+                },
+                zoom: zoomOptions,
             }
-        });
+        }
+    });
 
-        // Scatter chart of the number of days between a spec release and the homeserver
-        // supporting it.
-        const scatterContext = document.getElementById("spec-days-vs-support" + (post_rel ? "-post-rel" : ""));
-        new Chart(scatterContext, {
-            type: "scatter",
-            data: null,
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: "Days to support spec version" + (post_rel ? " (post-release)" : ""),
-                    },
-                    zoom: zoomOptions
-                }
-            },
-            plugins: [scatterDataLabels],
-        });
-    }
+    // Scatter chart of the number of days between a spec release and the homeserver
+    // supporting it.
+    const scatterContext = document.getElementById("spec-days-vs-support");
+    new Chart(scatterContext, {
+        type: "scatter",
+        data: null,
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Spec days vs. support days",
+                },
+                zoom: zoomOptions
+            }
+        },
+        plugins: [scatterDataLabels],
+    });
 
     const versionsSupportedContext = document.getElementById("supported-versions-over-time");
     new Chart(versionsSupportedContext, {
@@ -143,68 +138,64 @@ function build() {
 
 function render() {
     let allowedMaturities = ["stable", "beta", "alpha", "obsolete"].filter(maturity => document.getElementById(maturity).checked);
+    let displayAllReleases = !document.getElementById("display-all-releases").checked;
 
     fetch("data.json").then(response => response.json()).then(data => {
         // The full list of released spec versions.
         const specVersions = Object.keys(data.spec_versions.version_dates).sort(cmpVersions);
+        
+        var barDatasets = [];
+        var scatterDatasets = [];
 
-        // Generate the versions of each chart for each homeserver -- one including
-        // all data, and one including data for only after the homeserver's initial
-        // release.
-        for (const post_rel of [false, true]) {
-            var barDatasets = [];
-            var scatterDatasets = [];
+        for (let project in data.homeserver_versions) {
+            const projectVersions = data.homeserver_versions[project]["lag_" + (displayAllReleases ? "after_release" : "all")];
 
-            for (let project in data.homeserver_versions) {
-                const projectVersions = data.homeserver_versions[project]["lag_" + (post_rel ? "after_release" : "all")];
-
-                // If there are no versions, don't bother adding them.
-                if (!Object.keys(projectVersions).length) {
-                    continue;
-                }
-
-                // Filter projects by maturity.
-                if (!allowedMaturities.includes(data.homeserver_versions[project].maturity)) {
-                    continue
-                }
-
-                barDatasets.push({
-                    label: project,
-                    // Fill in zeros for missing spec versions.
-                    data: specVersions.map(v => projectVersions[v] || 0),
-                    borderWidth: 1,
-                })
-
-                scatterDatasets.push({
-                    label: project,
-                    data: Object.keys(projectVersions).map(v => {
-                        return {
-                            label: v,
-                            x: data.spec_versions.lag[v],
-                            y: projectVersions[v]
-                        };
-                    }),
-                    borderWidth: 1,
-                })
+            // If there are no versions, don't bother adding them.
+            if (!Object.keys(projectVersions).length) {
+                continue;
             }
 
-            // Bar chart of the number of days between a spec release and the homeserver
-            // supporting it.
-            const barChart = Chart.getChart("days-to-support" + (post_rel ? "-post-rel" : ""));
-            barChart.data = {
-                labels: specVersions,
-                datasets: barDatasets,
-            };
-            barChart.update();
+            // Filter projects by maturity.
+            if (!allowedMaturities.includes(data.homeserver_versions[project].maturity)) {
+                continue
+            }
 
-            // Scatter chart of the number of days between a spec release and the homeserver
-            // supporting it.
-            const scatterChart = Chart.getChart("spec-days-vs-support" + (post_rel ? "-post-rel" : ""));
-            scatterChart.data = {
-                datasets: scatterDatasets,
-            };
-            scatterChart.update();
+            barDatasets.push({
+                label: project,
+                // Fill in zeros for missing spec versions.
+                data: specVersions.map(v => projectVersions[v] || 0),
+                borderWidth: 1,
+            })
+
+            scatterDatasets.push({
+                label: project,
+                data: Object.keys(projectVersions).map(v => {
+                    return {
+                        label: v,
+                        x: data.spec_versions.lag[v],
+                        y: projectVersions[v]
+                    };
+                }),
+                borderWidth: 1,
+            })
         }
+
+        // Bar chart of the number of days between a spec release and the homeserver
+        // supporting it.
+        const barChart = Chart.getChart("days-to-support");
+        barChart.data = {
+            labels: specVersions,
+            datasets: barDatasets,
+        };
+        barChart.update();
+
+        // Scatter chart of the number of days between a spec release and the homeserver
+        // supporting it.
+        const scatterChart = Chart.getChart("spec-days-vs-support");
+        scatterChart.data = {
+            datasets: scatterDatasets,
+        };
+        scatterChart.update();
 
         // Line showing the dates when versions were supported.
         const now = new Date();
