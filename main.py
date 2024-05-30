b78e0b3f4b51fc22ea3102993924cd5293e2a4b0
Patrick Cloke <clokep@patrick.cloke.us>
2024-05-30 11:14:44 -0400

    Refactor getting repo data.

--- main.py
+++ main.py
@@ -136,6 +136,77 @@ def get_tag_datetime(tag: git.TagReference) -> datetime:
     )
 
 
+def get_spec_dates() -> (
+    tuple[dict[str, datetime], dict[str, datetime], dict[str, datetime]]
+):
+    # First get the known versions according to the spec repo.
+    spec_repo = get_repo("matrix-spec", "https://github.com/matrix-org/matrix-spec.git")
+
+    # Map of version -> commit date.
+    spec_versions = {
+        t.name.split("/")[-1]: get_tag_datetime(t)
+        for t in spec_repo.tags
+        if t.name.startswith("v")
+        or t.name.startswith("r")
+        or t.name.startswith("client_server/r")
+        or t.name.startswith("client-server/r")
+    }
+
+    # Map of room version -> commit date.
+    room_versions = {}
+    ROOM_VERSION_PATHS = [
+        "specification/rooms/",
+        "content/rooms/",
+        ":(exclude)content/rooms/fragments",
+    ]
+    ROOM_VERSION_FILE_PATTERN = re.compile(r".+/v(\d+)\.(?:md|rst)$")
+    for commit in spec_repo.iter_commits(paths=ROOM_VERSION_PATHS, reverse=True):
+        # Find the added files in the diff from the previous commit which match
+        # the expected paths.
+        # room_version_paths = [
+        #     d.b_path
+        #     for d in commit.parents[0].diff(commit, paths=ROOM_VERSION_PATHS)
+        #     if d.change_type == "A" and ROOM_VERSION_FILE_PATTERN.match(d.b_path)
+        # ]
+        for diff in commit.parents[0].diff(commit, paths=ROOM_VERSION_PATHS):
+            match = ROOM_VERSION_FILE_PATTERN.match(diff.b_path)
+            if diff.change_type == "A" and match:
+                room_version = match[1]
+                if room_version not in room_versions:
+                    room_versions[room_version] = commit.authored_datetime
+
+    # Map of default room versions -> commit date.
+    DEFAULT_ROOM_VERSION_PATHS = [
+        "specification/index.rst",
+        "content/_index.md",
+        "content/rooms/_index.md",
+    ]
+    default_room_versions = {}
+    for commit in spec_repo.iter_commits(
+        "origin/main", paths=DEFAULT_ROOM_VERSION_PATHS, reverse=True
+    ):
+        # Checkout this commit (why is this so hard?).
+        spec_repo.head.reference = commit
+        spec_repo.head.reset(index=True, working_tree=True)
+
+        cur_versions = get_versions_from_file(
+            spec_repo.working_dir,
+            DEFAULT_ROOM_VERSION_PATHS,
+            r"Servers MUST have Room Version (\d+)|Servers SHOULD use (?:\*\*)?room version (\d+)(?:\*\*)?",
+            [
+                # Dendrite incorrectly set room version 2 as the default for a period.
+                "2"
+            ],
+        )
+        assert len(cur_versions) <= 1, "Found more than one default room version"
+        if cur_versions:
+            default_room_version = next(iter(cur_versions))
+            if default_room_version not in default_room_versions:
+                default_room_versions[default_room_version] = commit.authored_datetime
+
+    return spec_versions, room_versions, default_room_versions
+
+
 def get_project_versions(
     project: ProjectMetadata,
     earliest_commit: str | None,
@@ -337,70 +408,8 @@ def main(
 
 
 if __name__ == "__main__":
-    # First get the known versions according to the spec repo.
-    spec_repo = get_repo("matrix-spec", "https://github.com/matrix-org/matrix-spec.git")
-
-    # Map of version -> commit date.
-    spec_versions = {
-        t.name.split("/")[-1]: get_tag_datetime(t)
-        for t in spec_repo.tags
-        if t.name.startswith("v")
-        or t.name.startswith("r")
-        or t.name.startswith("client_server/r")
-        or t.name.startswith("client-server/r")
-    }
-
-    # Map of room version -> commit date.
-    room_versions = {}
-    ROOM_VERSION_PATHS = [
-        "specification/rooms/",
-        "content/rooms/",
-        ":(exclude)content/rooms/fragments",
-    ]
-    ROOM_VERSION_FILE_PATTERN = re.compile(r".+/v(\d+)\.(?:md|rst)$")
-    for commit in spec_repo.iter_commits(paths=ROOM_VERSION_PATHS, reverse=True):
-        # Find the added files in the diff from the previous commit which match
-        # the expected paths.
-        # room_version_paths = [
-        #     d.b_path
-        #     for d in commit.parents[0].diff(commit, paths=ROOM_VERSION_PATHS)
-        #     if d.change_type == "A" and ROOM_VERSION_FILE_PATTERN.match(d.b_path)
-        # ]
-        for diff in commit.parents[0].diff(commit, paths=ROOM_VERSION_PATHS):
-            match = ROOM_VERSION_FILE_PATTERN.match(diff.b_path)
-            if diff.change_type == "A" and match:
-                room_version = match[1]
-                if room_version not in room_versions:
-                    room_versions[room_version] = commit.authored_datetime
-
-    # Map of default room versions -> commit date.
-    DEFAULT_ROOM_VERSION_PATHS = [
-        "specification/index.rst",
-        "content/_index.md",
-        "content/rooms/_index.md",
-    ]
-    default_room_versions = {}
-    for commit in spec_repo.iter_commits(
-        "origin/main", paths=DEFAULT_ROOM_VERSION_PATHS, reverse=True
-    ):
-        # Checkout this commit (why is this so hard?).
-        spec_repo.head.reference = commit
-        spec_repo.head.reset(index=True, working_tree=True)
-
-        cur_versions = get_versions_from_file(
-            spec_repo.working_dir,
-            DEFAULT_ROOM_VERSION_PATHS,
-            r"Servers MUST have Room Version (\d+)|Servers SHOULD use (?:\*\*)?room version (\d+)(?:\*\*)?",
-            [
-                # Dendrite incorrectly set room version 2 as the default for a period.
-                "2"
-            ],
-        )
-        assert len(cur_versions) <= 1, "Found more than one default room version"
-        if cur_versions:
-            default_room_version = next(iter(cur_versions))
-            if default_room_version not in default_room_versions:
-                default_room_versions[default_room_version] = commit.authored_datetime
+    # Get information about the spec itself.
+    spec_versions, room_versions, default_room_versions = get_spec_dates()
 
     # The final output data is an object:
     #
